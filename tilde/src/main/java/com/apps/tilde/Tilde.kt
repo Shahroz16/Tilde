fb871adcb36c9c84a72c65959b6896f4433a8a9e
Shahroz Ali <shrozali@gmail.com>
2017-05-18 18:04:55 +0500

    branch created

--- tilde/src/main/java/com/apps/tilde/Tilde.kt
+++ tilde/src/main/java/com/apps/tilde/Tilde.kt
@@ -8,17 +8,26 @@ typealias _t = Tilde
 const val TAG : String = "TILDE"
 object Tilde{
 
-    /// Creates an array of elements from the specified indexes, or keys, of the collection.
+    /// Creates an list of elements from the specified indexes, or keys, of the collection.
     /// Indexes may be specified as individual arguments or as arrays of indexes.
     ///
-    /// - parameter array: The array to source from
+    /// - parameter array: The list to source from
     /// - parameter indexes: Get elements from these indexes
-    /// - returns: New array with elements from the indexes specified.
+    /// - returns: New list with elements from the indexes specified.
     fun <T> at(array : List<T>, vararg indexes : Int) : List<T>{
         val results = ArrayList<T>()
         indexes.mapTo(results) { array[it] }
         return results;
     }
 
+    /// Create a copy of an List
+    ///
+    /// - parameter array: The List to copy
+    /// - returns: New copy of List
+    fun <T> copy(array : List<T>) : List<T>{
+        val results = ArrayList<T>()
+        array.mapTo(results) { it }
+        return results
+    }
 
 }
